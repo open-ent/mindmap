0196a6153789a50c94de199fe208a53330351de7
Clement Creusat <clement.creusat@gmail.com>
2024-01-09 17:59:59 +0100

    chore: update refacto explorer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
